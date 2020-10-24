f1ee6f85cb08944f45d53567699a10979347acc2
Badar396 <imdadabro345@gmail.com>
2020-10-23 22:29:45 -0700

    Added the functionality to open the admin screen if the application is not opened for the first time

--- Hospital.java
+++ Hospital.java
@@ -11,28 +11,42 @@ class Design
 	public ProcessBuilder clscr;
 	public Console console;
 	public String password;
-	public char delimiter;
+	public String delimiter;
+	public String str;
+	public String[] array;
 	public Design()
 	{
 		try
 		{
-			clscr=new ProcessBuilder("cmd","/c","cls");
-			clscr.inheritIO().start().waitFor();
+			clearScreen();
+			array=new String[3]; 
 			filepath=new File("E:\\JavaProjects\\JavaProjects\\check.txt");
 			filereader=new FileReader(filepath);
 			filewriter=new FileWriter(filepath,true);
 			reader=new BufferedReader(filereader);
 			writer=new BufferedWriter(filewriter);
 			input=new Scanner(System.in);
-			//System.out.println("Hello");
 			console=System.console();
-			delimiter=',';
+			delimiter=",";
 		}
 		catch(Exception e)
 		{
 			System.out.println("Exception: "+e.getMessage());
 
 		}
+	}
+
+	public void clearScreen()
+	{
+		clscr=new ProcessBuilder("cmd","/c","cls");
+		try
+		{
+			clscr.inheritIO().start().waitFor();
+	    }
+	    catch(Exception e)
+	    {
+	    	System.out.println("Exception"+e);
+	    }
 	}
 	public void userSelection()
 	{
@@ -102,9 +116,21 @@ class Design
 			System.out.println();
 		}
 	}
-	public void verifyCredentials()
+	public int verifyCredentials()
 	{
+		try
+		{
+			str=reader.readLine();
+		}
+		catch(Exception e){}
+		array=str.split(delimiter);
+		switch(array[0])
+		{
+			case "Admin":
+			return 1;
+		}
 
+		return 0;
 	}
 }
 class Admin extends Design
@@ -112,7 +138,7 @@ class Admin extends Design
 	public void adminTasks()
 	{
 		String userName=new String();
-		String str="Admin";
+		str="Admin";
 		System.out.print("Enter User Name: ");
 		userName=input.nextLine();
 		System.out.print(userName+", Enter Your Password: ");
@@ -126,6 +152,17 @@ class Admin extends Design
 	    }
 	    catch(Exception e)
 	    {}
+	}
+	public void mainScreen()
+	{
+		design("ADMIN");
+
+		System.out.println("\n\t\t\t\t1.Check Appointment");
+		System.out.println("\t\t\t\t2.Book Appointment");
+		System.out.println("\t\t\t\t3.Check Bill");
+
+		design("ADMIN");
+
 	}
 }
 class Doctor extends Design
@@ -152,10 +189,16 @@ public class Hospital
 		Doctor doctor=new Doctor();
 		Patient patient=new Patient();
 		Design design=new Design();
-
+		//admin.mainScreen();
 		if(!design.checkFirstTimeOpening())
 		{
-			design.verifyCredentials();
+			switch(design.verifyCredentials())
+			{
+	           case 1:
+	           admin.clearScreen();
+	           admin.mainScreen();
+	           break;
+			}
 		}
 		else
 		{
@@ -165,6 +208,8 @@ public class Hospital
 			{
 				case 1:
 				admin.adminTasks();
+				admin.clearScreen();
+				admin.mainScreen();
 				break;
 				case 2:
 				doctor.doctorTasks();
